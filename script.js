f4d01fc5fa7df223613bdc0100e8256fe48803ef
Muteeba Ahmed <132591988+ahmedmuteeba@users.noreply.github.com>
2023-10-15 03:31:21 +0500

    Add files via upload
    This is the initial version of site.

---
+++ script.js
@@ -0,0 +1,17 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener
+        ('click', () => {
+            removeActive();
+            card.classList.add('active');
+        }
+        )
+}
+)
+
+function removeActive() {
+    cards.forEach(card => {
+        card.classList.remove('active');
+    })
+}
